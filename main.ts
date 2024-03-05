94e5335b3412f5196339b13f9c69b7281bac3980
imranAleem <162370812+imranAleem@users.noreply.github.com>
2024-03-06 01:07:30 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//simple calculator
+//const first_num = 15;
+//const second_num = 5;
+//.log("the answer sum is :", first_num + second_num);
+//console.log("the answer subtract is :", first_num - second_num);
+//console.log("the answer mutiplication is :", first_num * second_num);
+//console.log("the answer division is :", first_num / second_num);
+
+//let n = 5;
+//console.log(n == 5)
+
+// num = 10;
+//console.log(num > 15);
+
+let num = 10;
+console.log(num != 5);
+
+
